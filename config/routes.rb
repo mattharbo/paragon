84e8f9e87241a5ffa77b7e6c6ddeb15d3a215399
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu2.home>
2021-12-02 20:12:44 +0100

    seasons controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'seasons/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'standalonepages/landing'
   root to: 'standalonepages#landing'
   resources :teams, only: [:index]
+  resources :seasons, only: [:index]
 end
