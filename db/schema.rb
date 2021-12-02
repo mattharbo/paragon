00971cd29648cbab7abaadfd2f976165c9c89549
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu2.home>
2021-12-02 20:07:19 +0100

    Season model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_175504) do
+ActiveRecord::Schema.define(version: 2021_12_02_190520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "seasons", force: :cascade do |t|
+    t.string "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
