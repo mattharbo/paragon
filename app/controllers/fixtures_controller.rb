75ebeae7b7f1de460f743c745eb8974f7d36322f
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu2.home>
2021-12-02 21:32:37 +0100

    Fixture controller show update + view

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -1,8 +1,17 @@
 class FixturesController < ApplicationController
+
+  before_action :set_fixture, only: [:show, :edit, :update, :destroy]
+
   def index
     @fixtures=Fixture.all
   end
 
   def show
+  end
+
+  private 
+
+  def set_fixture
+    @fixture = Fixture.find(params[:id])
   end
 end
