8842a2b35a81c46b25046d867a2d95f4c0a447e7
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu2.home>
2021-12-03 17:26:55 +0100

    Adding players controller + route + index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'standalonepages/landing'
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :competitions, only: [:index]
   resources :competseasons, only: [:index]
   resources :fixtures, only: [:index, :show]
+  resources :players, only: [:index]
 end
