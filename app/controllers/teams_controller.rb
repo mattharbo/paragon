40684e3c60ebd7541a1eb7445c018f0248ce3bac
Matthieu Harbonnier <matthieuharbonnier@macbook-pro-de-matthieu.home>
2022-05-07 01:00:11 +0200

    Creation of team show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,22 @@
 class TeamsController < ApplicationController
 
+    before_action :set_team, only: [:show]
+
     def index
         @teams=Team.all.order("id ASC")
+    end
+
+    def show
+
+        @home_games_goals=Event.where(selection:Selection.where(fixture:Fixture.where(hometeam:Team.find(params[:id]))))
+        @away_games_goals=Event.where(selection:Selection.where(fixture:Fixture.where(awayteam:Team.find(params[:id]))))
+        
+    end
+
+    private 
+
+    def set_team
+        @team=Team.find(params[:id])
     end
 
 end
