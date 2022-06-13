240dd16be6ad37b4cfb7c7736aa11546eac291ea
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu3.home>
2022-06-13 19:24:48 +0200

    layout of the fixture show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'staging' => 'standalonepages#staging'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'standalonepages/landing'
   root to: 'standalonepages#landing'
+  get 'staging' => 'standalonepages#staging'
+  get 'layout' => 'standalonepages#layout'
   resources :teams, only: [:index, :show, :edit, :update, :destroy]
   resources :seasons, only: [:index]
   resources :competitions, only: [:index]
@@ -14,7 +16,6 @@ Rails.application.routes.draw do
   resources :contracts, only: [:index]
   resources :selections, only: [:index, :edit, :update]
   resources :positions, only: [:index]
-  resources :standalonepages, only: [:staging]
   resources :eventtypes, only: [:index]
 
   # resources :events, only: [:index]
