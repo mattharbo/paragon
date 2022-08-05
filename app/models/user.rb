315433d01a77b839eb0b8ecb8a79b10b5c64802d
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu3.home>
2022-08-05 17:54:02 +0200

    Design Devise screens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable, :rememberable
 end
