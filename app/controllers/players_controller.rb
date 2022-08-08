22c6d3422f477b41b6241904894e5b54c82ba4a4
Matthieu Harbonnier <matthieuharbonnier@mbpdematthieu3.home>
2022-08-08 18:06:23 +0200

    Adding player stats

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,7 +10,64 @@ class PlayersController < ApplicationController
   end
 
   def show 
+    
     @player_contracts = Contract.where(player:@player)
+
+    @stats_of_arrays=[]
+
+    @player_contracts.each do |player_contract|
+
+      # 🚧🚧🚧🚧🚧 Competseason added manually!!!! 😳 🚧🚧🚧🚧🚧
+
+      number_of_team_games = team_fixtures_in_season(player_contract.team,Competseason.last).count
+
+      player_selections = Selection.where(contract:player_contract)
+      
+      involved_games=0
+      starter_games=0
+      minutes_of_play=0
+
+      player_contract_array = []
+
+
+      player_selections.each do |player_selection|
+
+      if player_selection.starter?
+          involved_games+=1
+          starter_games+=1
+
+          # Did starter play entirely the game or not? (90mn vs. until replacement)
+          if player_selection.substitutiontime.nil?
+            minutes_of_play+=90
+          else
+            minutes_of_play+=player_selection.substitutiontime          
+          end
+
+        else
+          # When did the substitute came in?
+          if player_selection.substitutiontime > 0
+            involved_games+=1
+            minutes_of_play+=(90-player_selection.substitutiontime)  
+          end
+        end 
+      end
+
+      pourcentage_of_games_started_when_involved=(starter_games/involved_games)*100
+
+      pourcentage_of_games_involved_for_the_team_this_season=(involved_games/number_of_team_games)*100
+
+      player_contract_array = [
+        number_of_team_games, 
+        involved_games, 
+        pourcentage_of_games_involved_for_the_team_this_season,
+        pourcentage_of_games_started_when_involved,
+        minutes_of_play]
+
+      @stats_of_arrays << player_contract_array
+
+    end
+    
+
   end
 
   def update
@@ -32,4 +89,11 @@ class PlayersController < ApplicationController
   def player_params
 	  params.require(:player).permit(:name, :firstname, :playerapiref)
   end
+
+  def team_fixtures_in_season(team,competseason)
+    # Total number of games for a given team in a given season
+    @team_season_games = Fixture.where(competseason:competseason).where(hometeam:team).or(Fixture.where(awayteam:team))
+    return @team_season_games
+  end
+
 end
